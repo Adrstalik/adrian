055100f8c6aaae81ede2a8b3a95f04be7109e8bf
adrian <Adrstalik@gmail.com>
2017-11-16 01:15:22 +0100

    added first sout

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void  Boom(){
+        int number = 5;
+        System.out.println("Your number is: "+number);
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.printf("");
+        Boom();
+        System.out.println("After add");
+    }
+
+
+}
