e880245739b6a90376b1ea9549d891954d5786b6
adrian <Adrstalik@gmail.com>
2017-11-16 01:26:47 +0100

    add sc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void  Boom(){
@@ -7,11 +9,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alibaba = sc.nextInt();
         System.out.println("");
         System.out.printf("");
         Boom();
         System.out.println("After add");
         System.out.println("Second sout");
+        if (alibaba >= 0) {
+            System.out.println("Number > " + alibaba);
+        }
+        else {
+            System.out.println("You fucked up. SOrry");
+        }
     }
 
 
